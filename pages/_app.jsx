81d8097260e2079fc16a6a38031fb81ae4427e64
ryan <tyryanosaur@gmail.com>
2024-07-08 09:31:50 -0500

    AOS working

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,15 +2,23 @@ import Layout from "../components/layout";
 import { MantineProvider } from '@mantine/core'
 
 import '../styles/global.css';
-
 import '@mantine/core/styles.css';
-
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+
 function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        Aos.init({
+            duration: 800,
+            once: false
+        });
+    }, []);
     return (
         <MantineProvider withGlobalStyles withNormalizeCSS>
             <Layout>
