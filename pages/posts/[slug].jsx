d3601191a67786a5d44af224db7c1b2f73bfd53d
ryan <tyryanosaur@gmail.com>
2024-07-11 22:56:07 -0500

    working on blog

---
+++ pages/posts/[slug].jsx
@@ -0,0 +1,108 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
+import { NextSeo } from 'next-seo';
+import Image from 'next/image';
+import Link from 'next/link';
+
+import {
+    ActionIcon,
+    Anchor,
+    Code,
+    Group,
+    Table as ReactTable,
+    Title,
+} from '@mantine/core';
+import { Prism } from '@mantine/prism';
+
+import { ArrowNarrowLeft } from 'tabler-icons-react';
+
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from 'rehype-slug';
+
+import { YouTube } from '@/components/index';
+
+import { PostMeta, getPostFromSlug, getSlugs } from '@/helpers/blog';
+
+import Routes from '@/config/routes';
+
+const components = {
+    Image,
+    YouTube,
+    ReactTable,
+    code: props => <Code {...props} />,
+    pre: props => {
+        const matches = (props.children.props.className || '').match(
+            /language-(?<lang>.*)/
+        );
+
+        return (
+            <Prism
+                language={
+                    matches && matches.groups && matches.groups.lang
+                        ? matches.groups.lang
+                        : ''
+                }
+                mb={20}
+            >
+                {props.children.props.children}
+            </Prism>
+        );
+    },
+    h1: props => <Title order={1} {...props} />,
+    h2: props => <Title order={2} {...props} />,
+    h3: props => <Title order={3} {...props} />,
+    h4: props => <Title order={4} {...props} />,
+    h5: props => <Title order={5} {...props} />,
+    h6: props => <Title order={6} {...props} />,
+    a: props => <Anchor {...props} />,
+};
+
+export default function PostPage({ post }) {
+    return (
+        <>
+            <NextSeo title={post.meta.title} description={post.meta.excerpt} />
+            <Group>
+                <ActionIcon
+                    component={Link}
+                    href={Routes.blog.href}
+                    aria-label='back to blog list'
+                >
+                    <ArrowNarrowLeft size={34} />
+                </ActionIcon>
+
+                <Title order={1}>{post.meta.title}</Title>
+            </Group>
+
+            <MDXRemote {...post.source} components={components} />
+
+            <GiscusComments />
+        </>
+    );
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+    const { slug } = params as { slug: string };
+    const { content, meta } = getPostFromSlug(slug);
+    const mdxSource = await serialize(content, {
+        mdxOptions: {
+            rehypePlugins: [
+                rehypeSlug,
+                [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+            ],
+        },
+    });
+
+    return { props: { post: { source: mdxSource, meta } } };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const paths = getSlugs().map((slug) => ({ params: { slug } }));
+
+    return {
+        paths,
+        fallback: false,
+    };
+};
