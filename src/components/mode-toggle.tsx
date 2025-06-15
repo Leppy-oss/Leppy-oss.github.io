0d7807bee77b67d33ab679e8db5d280fc32b29db
Leppy-oss <70586381+Leppy-oss@users.noreply.github.com>
2025-06-15 16:12:22 -0500

    switching to shadcn and magic-ui with dillionverma's port

---
+++ src/components/mode-toggle.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+
+export const ModeToggle = React.forwardRef<HTMLButtonElement, React.ComponentPropsWithoutRef<"button">>(
+  ({ className, ...props }, ref) => {
+    const { theme, setTheme } = useTheme();
+
+    return (
+      <Button
+        ref={ref}
+        variant="ghost"
+        type="button"
+        size="icon"
+        className={`px-2 ${className ?? ""}`}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        {...props}
+      >
+        <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
+        <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
+      </Button>
+    );
+  }
+);
+
+ModeToggle.displayName = "ModeToggle";
