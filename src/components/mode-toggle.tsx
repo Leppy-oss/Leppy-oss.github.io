cb244734a495da7d8a449d8d4ae6ff92e7fd3165
Leppy-oss <70586381+Leppy-oss@users.noreply.github.com>
2025-06-21 17:21:46 -0500

    update education + fix dark mode bug

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -16,8 +16,8 @@ export const ModeToggle = React.forwardRef<HTMLButtonElement, React.ComponentPro
         type="button"
         size="icon"
         className={`px-2 ${className ?? ""}`}
-        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
         {...props}
+        onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
         <SunIcon className="h-[1.2rem] w-[1.2rem] text-neutral-800 dark:hidden dark:text-neutral-200" />
         <MoonIcon className="hidden h-[1.2rem] w-[1.2rem] text-neutral-800 dark:block dark:text-neutral-200" />
